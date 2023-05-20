2ce5a98237180cf12b39a9005b7cca0e78ceb7cf
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-20 10:39:49 +0200

    Corrected exercise 2

--- Exercises. 1-10/02_Exercise.py
+++ Exercises. 1-10/02_Exercise.py
@@ -18,21 +18,15 @@ print(calculate())
   
    
    
-    def calculate():
-        numbers = []
-        for i in range(100):
-            if i % 5 == 0 or i % 7 == 0:
-                numbers.append(i)
-        total = sum(numbers)
+      def calculate():
+        total = 0
+        a = 0
+        b = 1
+        while a < 1000000:
+            if a % 2 == 0:
+                total += a
+            a, b = b, a + b
         return total
-     
-     
-    print(calculate())
-
-    # OR
-
-        def calculate():
-        return sum([i for i in range(100) if i % 5 == 0 or i % 7 == 0])
      
      
     print(calculate())
